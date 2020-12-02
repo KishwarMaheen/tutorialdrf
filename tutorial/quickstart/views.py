393fb5c3c9054cff196c9a1cb55072d05d381371
KishwarMaheen <34351287+KishwarMaheen@users.noreply.github.com>
2020-12-02 12:11:25 +0600

    Added viewsets.

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import UserSerializer, GroupSerializer
+from django.contrib.auth.models import User, Group
+
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
